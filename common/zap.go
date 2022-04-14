42b95c699aaf151847f99dfbe0c75b4dc5dd189f
wsjcko <wsjcko@163.com>
2022-04-14 22:30:33 +0800

    common/zap log

--- common/zap.go
+++ common/zap.go
@@ -1,6 +1,10 @@
 package common
 
 import (
+	// "fmt"
+	// "time"
+	// "os"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
@@ -13,29 +17,40 @@ var (
 func init() {
 	//日志文件名称
 	fileName := "micro.log"
-	syncWriter := zapcore.AddSync(
-		&lumberjack.Logger{
-			Filename: fileName, //文件名称
-			MaxSize:  512,      //MB
-			//MaxAge:     0,
-			MaxBackups: 0, //最大备份
-			LocalTime:  true,
-			Compress:   true, //是否启用压缩
-		})
+	// now := time.Now()
+	hook := &lumberjack.Logger{
+		Filename: fileName, //文件名称
+		// Filename: fmt.Sprintf("log/%s%04d%02d%02d%02d%02d%02d", fileName, now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second()), //filePath
+
+		MaxSize: 512, //MB
+		//MaxAge:     0, //文件最多保存多少天
+		MaxBackups: 0, //最大备份
+		LocalTime:  true,
+		Compress:   true, //是否启用压缩
+	}
+	defer hook.Close()
+	syncWriter := zapcore.AddSync(hook)
 	//编码
-	encoder := zap.NewProductionEncoderConfig()
+	encoder := zap.NewProductionEncoderConfig() //生产环境
+	// encoder := zap.NewDevelopmentEncoderConfig() //开发环境
 	//时间格式
 	encoder.EncodeTime = zapcore.ISO8601TimeEncoder
 	core := zapcore.NewCore(
 		// 编码器
 		zapcore.NewJSONEncoder(encoder),
-		syncWriter,
-		//
-		zap.NewAtomicLevelAt(zap.DebugLevel))
+		// zapcore.NewConsoleEncoder(encoder), //编码器配置
+		syncWriter, //打印到文件
+		// zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout),syncWriter), // 打印到控制台和文件
+		zap.NewAtomicLevelAt(zap.DebugLevel), //日志等级
+	)
 	log := zap.New(
 		core,
-		zap.AddCaller(),
-		zap.AddCallerSkip(1))
+		zap.AddCaller(),      //日志添加调用者信息
+		zap.AddCallerSkip(1), //设置skip，用户runtime.Caller的参数
+		// zap.AddStacktrace(zapcore.DebugLevel), //设置堆栈跟踪
+		zap.Fields(zap.String("serviceName", "ShopCart")), //设置初始化字段
+		// zap.Development(),//开发环境 panic开启文件及行号
+	)
 	logger = log.Sugar()
 }
 
